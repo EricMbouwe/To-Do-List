213426de55933c46ac4a75d673e9ee6f8ca97ca0
Azeem <azeemahmed101@gmail.com>
2020-08-21 15:32:15 +0200

    fix todo function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-import Todo from "./todo.js";
+import createTodo from './todo';
+
+const work = createTodo('work', 'today', '12-2-2020', 'low');
+const work2 = createTodo('work2', 'tmrw', '20-5-2010', 'high');
--- src/todo.js
+++ src/todo.js
@@ -1,23 +1,13 @@
-const Todo = (title, description, dueDate, init) => {
-  let priority = init * 2
+const createTodo = (title, description, dueDate, priority) => {
+  priority = priority || 'low';
 
-  // const setPriority = (newPriority) => priority += newPriority
-  function setPriority(x) {
-    date += x
-  }
+  const obj = { title, description, dueDate, priority };
 
-  const getTitle = () => title;
+  const setPriority = (priority) => {
+    obj.priority = priority;
+  };
 
-  return { priority, title, description, dueDate, setPriority, getTitle }
-}
+  return { obj, setPriority };
+};
 
-
-const work = Todo('work', 'today', '12-2-2020', 2)
-work.setPriority(10)
-
-
-console.log(work);
-console.log(work.priority);
-console.log(work.getTitle());
-
-export default Todo
+export default createTodo;
