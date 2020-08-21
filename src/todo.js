1e8af349711c9aba78a682befbea9f8973be4173
ericmbouwe <ericmbouwe@yahoo.fr>
2020-08-21 11:17:06 +0100

    todo module created

---
+++ src/todo.js
@@ -0,0 +1,23 @@
+const Todo = (title, description, dueDate, init) => {
+  let priority = init * 2
+
+  // const setPriority = (newPriority) => priority += newPriority
+  function setPriority(x) {
+    date += x
+  }
+
+  const getTitle = () => title;
+
+  return { priority, title, description, dueDate, setPriority, getTitle }
+}
+
+
+const work = Todo('work', 'today', '12-2-2020', 2)
+work.setPriority(10)
+
+
+console.log(work);
+console.log(work.priority);
+console.log(work.getTitle());
+
+export default Todo
